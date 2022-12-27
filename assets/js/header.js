723d94048f7fa51ffbb6d5383a806252fa820a89
BogdanAziev2003 <neazievbogdan@gmail.com>
2022-12-27 18:53:23 +0300

    header v 0.1

---
+++ assets/js/header.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector(".burger")
+let menu = document.querySelector(".menu__body")
+burger.addEventListener("click", ()=>{
+    console.log("dwdwdwdwd")
+    menu.classList.toggle("_active")
+})
